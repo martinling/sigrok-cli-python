6b6e8dbc5036506189931e42baeeebfe6162a448
Martin Ling <martin-git@earth.li>
2013-12-17 08:31:54 +0000

    Initial version. Supports -d, -O, --scan and --samples.

---
+++ sigrok-cli.py
@@ -0,0 +1,66 @@
+##
+## This file is part of the sigrok-cli-python project.
+##
+## Copyright (C) 2013 Martin Ling <martin-sigrok@earth.li>
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+from sigrok.core.classes import *
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--driver', help="The driver to use")
+parser.add_argument('-O', '--output-format', help="Output format", default="bits")
+parser.add_argument('--scan', help="Scan for devices", action='store_true')
+parser.add_argument('--samples', help="Number of samples to acquire", type=int)
+
+args = parser.parse_args()
+
+if not (args.scan or (args.driver and args.samples)):
+    parser.print_help()
+    sys.exit(1)
+
+context = Context()
+
+if args.scan:
+    for driver in context.drivers.values():
+        devices = driver.scan()
+        for device in devices:
+            print "%s - %s with %d probes: %s" % (device.driver.name, str.join(' ',
+                    [s for s in (device.vendor, device.model, device.version) if s]),
+                len(device.probes), str.join(' ', sorted(device.probes.keys())))
+    sys.exit(0)
+
+driver = context.drivers[args.driver]
+device = driver.scan()[0]
+
+if args.samples:
+    device.limit_samples = args.samples
+
+session = Session(context)
+session.add_device(device)
+
+output = Output(context.output_formats[args.output_format], device)
+
+def datafeed_in(device, packet):
+    text = output.receive(packet)
+    if text:
+        print text,
+
+session.add_callback(datafeed_in)
+session.start()
+session.run()
+session.stop()
